466926a542bf0e88559b5fb67bba5ec739e47ef8
Muhammad Afkar <afkar@justika.com>
2022-08-14 15:35:28 +0700

    sync components structure

--- src/containers/Home/components/WaveContentDivider/index.js
+++ src/containers/Home/components/WaveContentDivider/index.js
@@ -10,7 +10,8 @@ const Wrapper = styled.div`
 export default function WaveContentDivider() {
   return (
     <Wrapper>
-      <svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{ height: '100%', width: '100%' }}>
+      <svg className="wave-divider" viewBox="0 0 500 150" preserveAspectRatio="none" style={{ height: '100%', width: '100%' }}>
+        {/* https://smooth.ie/blogs/news/svg-wavey-transitions-between-sections */}
         <path d="M-17.21,65.63 C262.13,-75.48 323.08,153.46 522.85,72.54 L525.11,151.48 L-20.03,152.47 Z" style={{ stroke: 'none', fill: '#fff', opacity: 0.3 }} />
         <path d="M-15.52,74.51 C262.70,-43.91 323.08,153.46 523.42,93.26 L525.11,151.48 L-20.03,152.47 Z" style={{ stroke: 'none', fill: '#fff', opacity: 0.4 }} />
         <path d="M-21.72,81.42 C151.52,166.30 278.50,-80.41 536.40,102.14 L502.54,152.47 L-40.91,163.33 Z" style={{ stroke: 'none', fill: '#fff', opacity: 0.5 }} />
